08f2d8677a1b97db14dc257f19900ed27cd08c9e
angelique-df <faye.angelique.d@gmail.com>
2022-01-21 15:05:42 +0000

    removed unused lines in server.js

--- server.js
+++ server.js
@@ -1,7 +1,3 @@
-// server.js
-// where your node app starts
-
-// init project
 var express = require('express');
 var app = express();
 
@@ -11,21 +7,12 @@ var cors = require('cors');
 app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
 
 require('dotenv').config();
-
-// http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
-
-
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({ greeting: 'hello API' });
-});
-
 
 
 // listen for requests :)
