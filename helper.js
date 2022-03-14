dca4e6798f7bba1783761f2c741af6a87d3e13a0
angelique-df <faye.angelique.d@gmail.com>
2022-03-14 00:49:30 +0000

    Update helper.js

--- helper.js
+++ helper.js
@@ -1,17 +1,17 @@
 /**
- * 
+ *
  * @param {date} date the date built from new Date()
- * @returns an object with two keys: 
+ * @returns an object with two keys:
  * - unix: the date converted to unix
  * - utc: the date converted to utc
  */
 const matchingDates = (date) => {
-  return {
-    unix: date.getTime(),
-    utc: date.toUTCString()
-  }
+	return {
+		unix: date.getTime(),
+		utc: date.toUTCString(),
+	};
 };
 
 module.exports = {
-  matchingDates
-}
+	matchingDates,
+};
